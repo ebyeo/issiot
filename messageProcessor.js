52f666cf469a059d89a58a04fd2113669f7143ce
ebyeo <ebyeo69@gmail.com>
2017-06-26 17:49:04 +0800

    initial upload
    Clone from https://github.com/Azure-Samples/iot-hub-node-raspberrypi-client-app

---
+++ messageProcessor.js
@@ -0,0 +1,32 @@
+/*
+* IoT Hub Raspberry Pi NodeJS - Microsoft Sample Code - Copyright (c) 2017 - Licensed MIT
+*/
+'use strict';
+
+const PirSensor = require('./pirSensor.js');
+
+function MessageProcessor(option) {
+    this.inited = true;
+  });
+  this.sensor = new PirSensor(option.pirOption);
+  this.deviceId = option.deviceId;
+  this.inited = true;
+}
+
+MessageProcessor.prototype.getMessage = function (messageId, cb) {
+  if (!this.inited) { return; }
+  this.sensor.read((err, data) => {
+    if (err) {
+      console.log('[Sensor] Read data failed: ' + err.message);
+      return;
+    }
+
+    cb(JSON.stringify({
+      messageId: messageId,
+      deviceId: this.deviceId,
+      present: data
+    }), data);
+  });
+}
+
+module.exports = MessageProcessor;
---
+++ pirSensor.js
@@ -0,0 +1,18 @@
+/*
+* IoT Hub Raspberry Pi NodeJS - Microsoft Sample Code - Copyright (c) 2017 - Licensed MIT
+*/
+'use strict';
+
+const Gpio = require('onoff').Gpio;
+
+function Sensor(options) {
+  this.pir = new Gpio(options.pin, 'in', 'out');
+}
+
+Sensor.prototype.read = function (callback) {
+	this.pir.watch(function(err, value) {
+		callback(err, value);
+	});
+}
+
+module.exports = Sensor;
